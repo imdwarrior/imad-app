9f0d23f2336a5b1c5c024b8a29967e4357a23b53
Mohit Singh <ask4mohitdrocker@gmail.com>
2018-02-19 20:30:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get('/article-one', function (req, res){
  res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
 });
 app.get('/article-two', function (req, res){
-res.send('artcle two is requested and will be served here shortly') ;   
+ res.sendFile(path.join(__dirname, 'ui', 'article-two.html')); 
 });
 app.get('/article-three', function (req, res){
-res.send('artcle three is requested and will be served here shortly') ;   
+ res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
