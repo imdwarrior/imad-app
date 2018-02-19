96357901ee5693da050c86c08cdf7f7e53722e5a
Mohit Singh <ask4mohitdrocker@gmail.com>
2018-02-19 19:57:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article-one', function (req, res){
+res.send('artcle one is requested and will be served here shortly') ;   
+});
+app.get('/article-two', function (req, res){
+res.send('artcle two is requested and will be served here shortly') ;   
+});
+app.get('/article-three', function (req, res){
+res.send('artcle three is requested and will be served here shortly') ;   
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
