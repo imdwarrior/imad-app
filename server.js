c3fd58204ba7f95677fe9162fd0824a1ab658ba3
Mohit Singh <ask4mohitdrocker@gmail.com>
2018-02-20 15:53:39 +0530

    changed locally
    server.js
    article 1,2,3 html linked to common css file

--- server.js
+++ server.js
@@ -2,15 +2,91 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var articles = {
+	var 'article-one' = {
+		title	 :  'Article One | Mohit' ,
+		date	 : 'Feb 17, 2018' , 
+		heading	:  'Article One',
+		content	 : `<p>
+		                    This is the content for Article one This is the ontent for Article one This is the ontent for Article one This is the ontent for Article one
+		                    This is the content for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article one
+		                    
+		                </p>
+		            <p>
+		                    This is the content for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article one
+		            </p>`, 
+		};
+
+	var 'article-two' = {
+			title	 :  'Article Two | Mohit' ,
+			date	 : 'Feb 18, 2018' , 
+			heading	:  'Article Two',
+			content	 : `<p>
+			                    This is the content for Second Article
+			            </p>`, 
+		};
+
+	var 'article-three'
+		 = {
+			title	 :  'Article Three | Mohit' ,
+			date	 : 'Feb 18, 2018' , 
+			heading	:  'Article Three',
+			content	 : `<p>
+			                    This is the content for Third Article
+			            </p>`, 
+		};
+
+};
+
+
+function createTemplate(data){
+	var title = data.title;
+	var date = data.date;
+	var heading = data.heading;
+	var content = data.content;
+	var htmlTemplate = `
+	<html>
+	<head>
+	    <link href=style.css rel="stylesheet"/>
+	        <title>
+	         ${title}
+	        </title>
+	    </head>
+	    <body>
+	        <div>
+	      <a href="/">Home</a>
+	    </div>
+	    <hr/>
+	    <h3>
+	        ${heading}
+	    </h3>
+	    <div>
+	        ${date}
+	    </div>
+	    <div>
+	    <p>
+	         ${content}
+	    </p>
+	    </div>
+	    </body>
+	    
+	</html>
+	`;
+	return htmlTemplate;
+}
+
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one', function (req, res){
- res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
+app.get('/:articleName', function (req, res){
+ res.send(createTemplate(articles[articleName])); 
 });
+
+var articleName = req.param.articleName;
+
 app.get('/article-two', function (req, res){
  res.sendFile(path.join(__dirname, 'ui', 'article-two.html')); 
 });
