d888cb9d16d218cedc7b4d063c6d062bea1b9159
Mohit Singh <ask4mohitdrocker@gmail.com>
2018-02-23 09:58:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -46,8 +46,8 @@ request.onreadystatechange = function() {
  	//captures the name and render it as a list
  	var names = ['name1','name2','name3','name4'];
  	var list = '';
- 	for(var i=0,i<names.length;i++){
- 		list+= '<li>' + names[i] + <'/li'> ;
+ 	for(var i=0;i<names.length;i++){
+ 		list+= '<li>' + names[i] + '</li'> ;
  	}
  	var ul = document.getElementById('namelist');
  	ul.innerHTML = list;
