6c68cbedd4fc3638bf4c6da33724d48654fe6580
Mohit Singh <ask4mohitdrocker@gmail.com>
2018-02-24 11:08:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,15 +110,16 @@ app.get('/', function (req, res) {
 
 app.get('/test-db',function(req,res){
 	//make a query
+		//return a response
 	pool.query('SELECT * FROM test',function(err,res){
 		if(err){
-			res.status(500).send(err.toString());
+			res.statusCode(500).send(err.toString());
 		}
 		else{
 			res.send(JSON.stringify(result.rows));
 		}
 	});
-	//return a response
+
 });
 
 app.get('/:articleName', function (req, res){
