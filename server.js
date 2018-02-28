37f8158b18b6c7071add33b07e773711cbf61c0f
Mohit Singh <ask4mohitdrocker@gmail.com>
2018-02-28 19:07:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ function hash(input, salt){
   return hashed.toString('hex');
 }
 
-app.get('/hash:/input', function(req, res){
+app.get('/hash/:input', function(req, res){
 	var hashedString = hash(req.params.input, 'random-salt');
 	res.send(hashedStrings);
 });
