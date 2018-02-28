ac5dec41a0aeb394a578ef38470e814307015094
Mohit Singh <ask4mohitdrocker@gmail.com>
2018-02-28 19:01:41 +0530

    crypto check

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config ={
 	user: 'ask4mohitdrocker',
@@ -52,6 +53,18 @@ function createTemplate(data){
 	`;
 	return htmlTemplate;
 }
+
+
+function hash(input, salt){
+  //create a hash
+  var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512 , 'sha512');
+  return hashed.toString('hex');
+}
+
+app.get('/hash:/input', function(req, res){
+	var hashedString = hash(req.params.input, 'random-salt');
+	res.send(hashedStrings);
+});
 
 var counter=0;
 app.get('/counter', function (req, res) {
