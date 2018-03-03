ed6560e6ebde70de0046287cc2bc58050e2e7afd
Mohit Singh <ask4mohitdrocker@gmail.com>
2018-03-03 16:26:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -138,6 +138,11 @@ app.get('/check-login', function(req,res){
   }
 });
 
+app.get('/logout', function(req,res){
+  delete req.session.auth;
+  res.send("Logged out Succesfully")
+});
+
 var counter=0;
 app.get('/counter', function (req, res) {
   counter+=1;
